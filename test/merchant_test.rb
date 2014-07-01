6fa245e6c8d7c1c9bb934d717db145062ddd78e9
Jonmichael Chambers <jon@tyrbo.net>
2014-07-01 15:21:57 -0600

    Adds a data method to MerchantTest so we are not repeating ourselves

--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -4,8 +4,11 @@ require_relative '../lib/item_repository'
 require_relative '../lib/item'
 
 class MerchantTest < MiniTest::Test
+  def data
+    { id: 1, name: 'Best Buy', created_at: Time.now.to_s, updated_at: Time.now.to_s }
+  end
+
   def test_can_create_a_merchant
-    data = { id: 1, name: 'Best Buy', created_at: Time.now.to_s, updated_at: Time.now.to_s }
     m = Merchant.new(data)
 
     assert_equal 1, m.id
@@ -16,7 +19,6 @@ class MerchantTest < MiniTest::Test
 
   def test_we_can_find_items_belonging_to_merchant
     ItemRepository.load('test/fixtures/items.csv', Item)
-    data = { id: 1, name: 'Best Buy', created_at: Time.now.to_s, updated_at: Time.now.to_s }
     items = Merchant.new(data).items
     assert_equal 2, items.count
 
