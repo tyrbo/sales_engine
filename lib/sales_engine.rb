4f56b0035b37328d025319e2688de071d9d4d0d6
Jonmichael Chambers <jon@tyrbo.net>
2014-07-09 15:28:47 -0600

    Fixes SalesEngine to use the new repo setup

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -11,19 +11,21 @@ require_relative 'invoice_item'
 require_relative 'merchant'
 require_relative 'transaction'
 require_relative 'invoice'
+require_relative 'csv_loader'
 
 class SalesEngine
   include RepositoryAccessors
 
-  def initialize(dir)
+  def initialize(csv_dir)
+    @csv_dir = csv_dir
   end
 
   def startup
-    merchant_repository.load('data/merchants.csv', Merchant)
-    item_repository.load('data/items.csv', Item)
-    customer_repository.load('data/customers.csv', Customer)
-    invoice_repository.load('data/invoices.csv', Invoice)
-    invoice_item_repository.load('data/invoice_items.csv', InvoiceItem)
-    transaction_repository.load('data/transactions.csv', Transaction)
+    merchant_repository.load     CSVLoader.new('data/merchants.csv'), Merchant
+    item_repository.load         CSVLoader.new('data/items.csv'), Item
+    customer_repository.load     CSVLoader.new('data/customers.csv'), Customer
+    invoice_repository.load      CSVLoader.new('data/invoices.csv'), Invoice
+    invoice_item_repository.load CSVLoader.new('data/invoice_items.csv'), InvoiceItem
+    transaction_repository.load  CSVLoader.new('data/transactions.csv'), Transaction
   end
 end
