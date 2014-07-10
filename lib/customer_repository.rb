0dd7f03acbf187250860f23cbd99fe4614277c8e
Jonmichael Chambers <jon@tyrbo.net>
2014-07-10 08:34:37 -0600

    Refactors some things in the CustomerRepo

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -4,17 +4,23 @@ class CustomerRepository < Repository
 
   def self.most_items
     all.max_by do |customer|
-      customer.invoices.select(&:successful?)
-                       .flat_map(&:invoice_items)
-                       .inject(0) { |sum, x| sum + x.quantity }
+      successful_items(customer)
+        .inject(0) { |sum, x| sum + x.quantity }
     end
   end
 
   def self.most_revenue
     all.max_by do |customer|
-      customer.invoices.select(&:successful?)
-                       .flat_map(&:invoice_items)
-                       .inject(0) { |sum, x| sum + (x.quantity * x.unit_price) }
+      successful_items(customer)
+        .inject(0) { |sum, x| sum + (x.quantity * x.unit_price) }
     end
+  end
+
+  private
+
+  def self.successful_items(customer)
+      customer.invoices
+              .select(&:successful?)
+              .flat_map(&:invoice_items)
   end
 end
--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -8,7 +8,7 @@ class InvoiceRepository < Repository
   def self.create(args)
     new_invoice_id = all.max_by(&:id).id + 1
     time = Time.now.to_s
-    invoice_item_repository.create_all(new_invoice_id, Array(args[:items]))
+    invoice_item_repository.create_all(new_invoice_id, args[:items])
     invoice = Invoice.new(id: new_invoice_id, customer_id: args[:customer].id, merchant_id: args[:merchant].id, status: args[:status], created_at: time, updated_at: time)
     all << invoice
     invoice
